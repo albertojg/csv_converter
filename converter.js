60d1aa383831bab93f1ef3b94d35c09a95392930
Alberto Gosal <alberto.gosal@gmail.com>
2015-11-15 18:47:50 +0800

    Add support for translation file

--- converter.js
+++ converter.js
@@ -1,7 +1,8 @@
 var argv = require('minimist')(process.argv.slice(2)),
     fs = require('fs'),
     xml2js = require('xml2js'),
-    csv = require('fast-csv');
+    csv = require('fast-csv'),
+    async = require('async');
 
 var source = argv.f || argv.file;
 var output = argv.o || argv.output;
@@ -10,38 +11,61 @@ if (!source || !output) {
   process.exit(0);
 }
 
+var options = getAdditionalOptions();
+
 switch (argv._[0]) {
   case 'jsonToCsv':
-    convertJsonToCsv(source, output);
+    convertJsonToCsv(source, output, options);
     break;
 
   case 'csvToJson':
-    convertCsvToJson(source, output);
+    convertCsvToJson(source, output, options);
     break;
 
   case 'xmlToCsv':
-    convertXmlToCsv(source, output);
+    convertXmlToCsv(source, output, options);
     break;
 
   case 'csvToXml':
-    convertCsvToXml(source, output);
+    convertCsvToXml(source, output, options);
     break;
 
   default:
     console.log('Unknown command type [jsonToCsv, csvToJson]');
 }
 
-function convertJsonToCsv(source, output) {
-  console.log('Converting JSON source file: ' + source + " to CSV output file: " + output);
-
-  var json = require(source);
-  var flatJson = flattenJSONKey(json);
-  writeToCsv(flatJson, output);
+function getAdditionalOptions() {
+  return {
+    translationSource: argv.t || argv.translation,
+    csvFieldValue: argv.v
+  };
 }
 
-function convertCsvToJson(source, output) {
-  console.log('Converting CSV source file: ' + source + " to JSON output file: " + output);
- 
+function convertJsonToCsv(source, output, options) {
+  options = options || {};
+
+  var sourceJson = require(source);
+  sourceJson = flattenJSONKey(sourceJson);
+
+  var translationJson = {};
+  if (options.translationSource) {
+    translationJson = require(options.translationSource);
+    translationJson = flattenJSONKey(translationJson);
+  }
+
+  var result = [];
+  for (key in sourceJson) {
+    result.push({
+      key: key,
+      string_value: sourceJson[key],
+      translated_value: translationJson[key]
+    });
+  }
+
+  writeToCsv(result, output);
+}
+
+function convertCsvToJson(source, output, options) {
   function callback(data) {
     var unflatJson = unflattenJSONKey(data);
     var content = "module.exports = " + JSON.stringify(unflatJson, null, 4) + ";";
@@ -50,36 +74,66 @@ function convertCsvToJson(source, output) {
     console.log('Successfully write to JSON file: ' + output + '!');
   };
 
-  readFromCsv(source, callback);  
+  readFromCsv(source, callback, options);
 }
 
-function convertXmlToCsv(source, output) {
-  console.log('Converting XML source file: ' + source + " to CSV output file: " + output);
+function convertXmlToCsv(source, output, options) {
+  options = options || {};
 
   var parser = new xml2js.Parser({
     explicitArray: true,
     preserveChildrenOrder: true
   });
-  fs.readFile(source, function(err, data) {
-    parser.parseString(data, function(err, xml) {
-      if (err) {
-        console.log('Failed to parse XML input file: ' + source + '! Error: ' + err);
-        process.exit(0);
-      }
 
-      if (!xml || !xml.resources || !xml.resources.string) {
-        console.log('Empty resources element! Nothing to convert..');
-        process.exit(0);
+  async.series({
+    sourceXml: function(callback) {
+      fs.readFile(source, function(err, data) {
+        parser.parseString(data, callback);
+      });
+    },
+    translationXml: function(callback) {
+      if (options.translationSource) {
+        fs.readFile(options.translationSource, function(err, data) {
+          parser.parseString(data, callback);
+        });
+      } else {
+        callback(null, null);
       }
+    }
+  }, function(err, result) {
+    if (err) {
+      console.log('Failed to read and parse XML input file! Error: ' + err);
+      process.exit(0);
+    }
 
-      var object = {};
-      xml.resources.string.forEach(function(item) {
+    var sourceXml = result.sourceXml;
+    var translationXml = result.translationXml;
+
+    if (!sourceXml || !sourceXml.resources || !sourceXml.resources.string) {
+      console.log('Empty source XML file!');
+      process.exit(0);
+    }
+
+    var translationMap = {};
+    if (translationXml && translationXml.resources && translationXml.resources.string) {
+      translationXml.resources.string.forEach(function(item) {
         var key = item.$.name;
         var value = item._;
-        object[key] = value;
+        translationMap[key] = value;
       });
-      writeToCsv(object, output);
+    }
+
+    var result = [];
+    sourceXml.resources.string.forEach(function(item) {
+      var key = item.$.name;
+      result.push({
+        key: key,
+        string_value: item._,
+        translated_value: translationMap[key]
+      });
     });
+
+    writeToCsv(result, output);
   });
 }
 
@@ -89,7 +143,6 @@ function convertCsvToXml(source, output) {
   function callback(data) {
     var builder = new xml2js.Builder({
       rootName: 'resources',
-      // headless: true,
       cdata: true
     });
 
@@ -112,15 +165,17 @@ function convertCsvToXml(source, output) {
   readFromCsv(source, callback);
 }
 
-function readFromCsv(source, completeCallback) {
+function readFromCsv(source, completeCallback, options) {
+  options = options || {};
+  var valueField = options.csvFieldValue || 'string_value';
+
   var data = {};
   csv
     .fromPath(source, {
       headers: true
     })
     .on('data', function(row) {
-      data[row.key] = row.translated_value;
-      // data[row.key] = row.string_value;
+      data[row.key] = row[valueField];
     })
     .on('end', function() {
       if (completeCallback) {
@@ -139,9 +194,9 @@ function writeToCsv(data, destination) {
       quoteColumns: true,
       transform: function(key) {
         return {
-          'key': key,
-          'string_value': data[key],
-          'translated_value': '',
+          'key': data[key].key,
+          'string_value': data[key].string_value,
+          'translated_value': data[key].translated_value,
           'remarks': ''  
         };
       }
